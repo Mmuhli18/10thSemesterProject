feddc1fa1afbfe4169374c920804e0b2afa2a676
Mmuhli18 <56340057+Mmuhli18@users.noreply.github.com>
2023-03-10 10:24:15 +0100

    Added file explorer

--- Assets/Scripts/ViewportHandler.cs
+++ Assets/Scripts/ViewportHandler.cs
@@ -2,16 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using AnotherFileBrowser.Windows;
+using UnityEngine.Networking;
 
 public class ViewportHandler : MonoBehaviour
 {
     public Renderer planeRenderer;
     public Material defaultMaterial;
     public Material footageMaterial;
+    private Button footageButton;
 
     public void AddFootage(Button button)
     {
-        planeRenderer.material = footageMaterial;
-        button.style.display = DisplayStyle.None;
+        footageButton = button;
+        var bp = new BrowserProperties();
+        bp.filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+        bp.filterIndex = 0;
+
+        new FileBrowser().OpenFileBrowser(bp, path =>
+        {
+            //Load image from local path with UWR
+            StartCoroutine(LoadFootage(path));
+        });
+    }
+
+    IEnumerator LoadFootage(string path)
+    {
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(path))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log(uwr.error);
+                planeRenderer.material = defaultMaterial;
+            }
+            else
+            {
+                var uwrTexture = DownloadHandlerTexture.GetContent(uwr);
+                footageMaterial.mainTexture = uwrTexture;
+                planeRenderer.material = footageMaterial;
+                footageButton.style.display = DisplayStyle.None;
+            }
+        }
     }
 }
