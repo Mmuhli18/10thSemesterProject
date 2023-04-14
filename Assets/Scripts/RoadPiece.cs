0ea8c42d89eae4343c08c33de8c2dd615299f3df
Mmuhli18 <56340057+Mmuhli18@users.noreply.github.com>
2023-04-14 16:29:04 +0200

    Experimented with sidewalk meshes

--- Assets/Scripts/RoadPiece.cs
+++ Assets/Scripts/RoadPiece.cs
@@ -17,6 +17,8 @@ public class RoadPiece : MonoBehaviour
     public LineRenderer lineRenderer;
     [HideInInspector]
     public MeshFilter roadMesh;
+    MeshFilter sideMeshRight;
+    MeshFilter sideMeshLeft;
 
     private void Awake()
     {
@@ -36,12 +38,22 @@ public class RoadPiece : MonoBehaviour
         lineRenderer.positionCount = 2;
         lineRenderer.SetPosition(0, startDot.transform.position);
         lineRenderer.SetPosition(1, endDot.transform.position);
+        MeshFace roadFace = FaceFromRoadPiece();
         if (roadMesh == null)
         {
             roadMesh = Instantiate(roadMeshPrefab).GetComponent<MeshFilter>();
+
+            sideMeshLeft = Instantiate(roadMeshPrefab).GetComponent<MeshFilter>();
+            sideMeshLeft.transform.position += settings.roadDirection.normalized * settings.width;
+            sideMeshLeft.transform.parent = roadMesh.transform;
+
+            sideMeshRight = Instantiate(roadMeshPrefab).GetComponent<MeshFilter>();
+            sideMeshRight.transform.position -= settings.roadDirection.normalized * settings.width;
+            sideMeshRight.transform.parent = roadMesh.transform;
+
             roadMesh.transform.position -= new Vector3(0f, 0f, -1f);
         }
-        MeshFace face = MeshDrawer.combineFaces(MeshDrawer.extrudeFace(FaceFromRoadPiece(), 1f, new Vector3(0f, 0f, -1f)));
+        MeshFace face = MeshDrawer.combineFaces(MeshDrawer.extrudeFace(roadFace, 1f, new Vector3(0f, 0f, -1f)));
         roadMesh.mesh = MeshDrawer.makeMesh(face);
     }
 
@@ -51,31 +63,58 @@ public class RoadPiece : MonoBehaviour
         lineRenderer.SetPosition(0, startDot.transform.position);
         lineRenderer.SetPosition(1, endPos);
         tempEndPos = endPos;
+        MeshFace roadFace = FaceFromRoadPiece();
         if (roadMesh == null)
         {
             roadMesh = Instantiate(roadMeshPrefab).GetComponent<MeshFilter>();
+
+            sideMeshLeft = Instantiate(roadMeshPrefab).GetComponent<MeshFilter>();
+            sideMeshLeft.transform.position += settings.roadDirection.normalized * settings.width;
+            sideMeshLeft.transform.parent = roadMesh.transform;
+
+            sideMeshRight = Instantiate(roadMeshPrefab).GetComponent<MeshFilter>();
+            sideMeshRight.transform.position -= settings.roadDirection.normalized * settings.width;
+            sideMeshRight.transform.parent = roadMesh.transform;
+
             roadMesh.transform.position -= new Vector3(0f, 0f, -1f);
         }
-        roadMesh.mesh = MeshDrawer.makeMesh(MeshDrawer.combineFaces(MeshDrawer.extrudeFace(FaceFromRoadPiece(), 1f, new Vector3(0f, 1f, 0f))));
+        roadMesh.mesh = MeshDrawer.makeMesh(MeshDrawer.combineFaces(MeshDrawer.extrudeFace(roadFace, 1f, new Vector3(0f, 1f, 0f))));
+        sideMeshLeft.mesh = MeshDrawer.makeMesh(MeshDrawer.combineFaces(MeshDrawer.extrudeFace(SideWalkFaceFromRoadPiece(-1), 2f, new Vector3(0f, 1f, 0f))));
+        sideMeshRight.mesh = MeshDrawer.makeMesh(MeshDrawer.combineFaces(MeshDrawer.extrudeFace(SideWalkFaceFromRoadPiece(1), 2f, new Vector3(0f, 1f, 0f))));
     }
 
     MeshFace FaceFromRoadPiece()
     {
-        Vector3 startpoint = startDot.transform.position;
-        Vector3 endpoint;
-        if (endDot == null) endpoint = tempEndPos;
-        else endpoint = endDot.transform.position;
-        Vector3 direction = (startpoint - endpoint).normalized;
-        direction = Quaternion.AngleAxis(90f, Vector3.back) * direction;
+        settings.startPoint = startDot.transform.position;
+        if (endDot == null) settings.endPoint = tempEndPos;
+        else settings.endPoint = endDot.transform.position;
+        Vector3 direction = (settings.startPoint - settings.endPoint).normalized;
+        settings.roadDirection = Quaternion.AngleAxis(90f, Vector3.back) * direction;
 
         List<Vector3> corners = new List<Vector3>();
-        Vector3 offset1 = direction.normalized * settings.width;
-        Vector3 offset2 = -direction.normalized * settings.width;
+        Vector3 offset1 = settings.roadDirection.normalized * settings.width;
+        Vector3 offset2 = -settings.roadDirection.normalized * settings.width;
         Vector3 movement = new Vector3(20f, 20f, 0f);
-        corners.Add(new Vector3(startpoint.x, startpoint.y, 0f) + offset1 + movement);
-        corners.Add(new Vector3(startpoint.x, startpoint.y, 0f) + offset2 + movement);
-        corners.Add(new Vector3(endpoint.x, endpoint.y, 0f) + offset2 + movement);
-        corners.Add(new Vector3(endpoint.x, endpoint.y, 0f) + offset1 + movement);
+        corners.Add(new Vector3(settings.startPoint.x, settings.startPoint.y, 0f) + offset1 + movement);
+        corners.Add(new Vector3(settings.startPoint.x, settings.startPoint.y, 0f) + offset2 + movement);
+        corners.Add(new Vector3(settings.endPoint.x, settings.endPoint.y, 0f) + offset2 + movement);
+        corners.Add(new Vector3(settings.endPoint.x, settings.endPoint.y, 0f) + offset1 + movement);
+
+        return MeshDrawer.drawFace(corners, true);
+    }
+
+    MeshFace SideWalkFaceFromRoadPiece(int direction)
+    {
+        Vector3 sideWalkOffset = settings.roadDirection.normalized * settings.width * direction;
+
+        List<Vector3> corners = new List<Vector3>();
+        Vector3 offset1 = settings.roadDirection.normalized * settings.SideWalkWidth;
+        Vector3 offset2 = -settings.roadDirection.normalized * settings.SideWalkWidth;
+        Vector3 movement = new Vector3(20f, 20f, 0f);
+        corners.Add(new Vector3(settings.startPoint.x, settings.startPoint.y, 0f) + offset1 + movement + sideWalkOffset);
+        corners.Add(new Vector3(settings.startPoint.x, settings.startPoint.y, 0f) + offset2 + movement + sideWalkOffset);
+        corners.Add(new Vector3(settings.endPoint.x, settings.endPoint.y, 0f) + offset2 + movement + sideWalkOffset);
+        corners.Add(new Vector3(settings.endPoint.x, settings.endPoint.y, 0f) + offset1 + movement + sideWalkOffset);
 
         return MeshDrawer.drawFace(corners, true);
     }
@@ -90,4 +129,7 @@ public class RoadSettingHolder
     public float BikeLaneWidth = 10f;
     public bool hasSideWalks = false;
     public float SideWalkWidth = 10f;
+    public Vector3 roadDirection = new Vector3(1f, 0f, 0f);
+    public Vector3 startPoint;
+    public Vector3 endPoint;
 }
