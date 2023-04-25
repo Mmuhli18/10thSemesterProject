f717ac1bc119f3e69cd3703b34f873360c919fdf
MikkelKristensen <nicemikkel@hotmail.com>
2023-04-25 10:25:38 +0200

    ShadowReceiver no longer set inactive if road not transparent

--- Assets/Simulation/Scripts/Road.cs
+++ Assets/Simulation/Scripts/Road.cs
@@ -111,7 +111,7 @@ public class Road : MonoBehaviour
         }
         else
         {
-            shadowReceiver.SetActive(false);
+            //shadowReceiver.SetActive(false);
             baseModel.SetActive(true);
         }
 
